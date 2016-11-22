9f44b15bf3bd21feabbb6696deaf29d2fef37b28
SolalDR <solal.dussout-revel@hotmail.fr>
2016-11-22 23:26:18 +0100

    Implement Devise / Implement Devise-Bootstrap View / Overrive Devise Views / Override Devise Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "/logout" => "devise/sessions#destroy"
+  end
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#home"
+  root to: "application#home"
+
+
 end
