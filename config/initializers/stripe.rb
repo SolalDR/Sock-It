a125d67644a63393c453eadfec83f0f587f8f09d
SolalDR <solal.dussout-revel@hotmail.fr>
2017-01-08 13:27:44 +0100

    Change stripe config for production env

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,14 @@
-Rails.configuration.stripe = {
-    :publishable_key => Rails.application.secrets.stripe_publishable_key,
-    :secret_key      => Rails.application.secrets.stripe_secret_key
-}
+
+if Rails.env.production?
+  Rails.configuration.stripe = {
+      :publishable_key => ENV["STRIPE_PUBLISHABLE_KEY"],
+      :secret_key      => ENV["STRIPE_SECRET_KEY"]
+  }
+else
+  Rails.configuration.stripe = {
+      :publishable_key => Rails.application.secrets.stripe_publishable_key,
+      :secret_key      => Rails.application.secrets.stripe_secret_key
+  }
+end
 
 Stripe.api_key = Rails.application.secrets.stripe_secret_key
